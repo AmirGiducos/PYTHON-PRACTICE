be4ce8e8ae2d91751a8198a2a0b677e1aace5620
AmirGiducos <amirgiducos@gmail.com>
2025-12-09 20:08:48 +0800

    Update 1study1.py

--- loops/1study1.py
+++ loops/1study1.py
@@ -1,3 +1,21 @@
-# for loop, lists
+def main():
+    number = numberCounter()
+    doTask(number)
 
-for
+    
+def numberCounter():
+    while True:
+        try:
+            n = int(input("Enter amount of times you want to do task (Enter positive number): "))
+            if n > 0:
+                return n
+            print("Please enter a positive number.")
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+
+def doTask(n):
+    for i in range(n):
+        print("Task finished...")
+
+
+main()
